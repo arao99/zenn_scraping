4922b0a4e1e66d5446b2283f4f974f23c4134ab8
arao99 <yusuque.swallows@gmail.com>
2021-01-10 17:24:31 +0900

    not get NEW

--- 2nd_article/first_scraping_comment4.rb
+++ 2nd_article/first_scraping_comment4.rb
@@ -10,6 +10,6 @@ html = URI.open(url).read
 doc = Nokogiri::HTML.parse(html)
 
 # ニュースの見出しを取得して出力する
-doc.at_css('section#tabpanelTopics1 ul').css('li').each do |li|
-  puts li.text.strip
+doc.at_css('section#tabpanelTopics1 ul').css('h1').each do |h1|
+  puts h1.text.strip
 end
